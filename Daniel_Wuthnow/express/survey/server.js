1161d42714c83c8abf0b6103a7541c2db7c87ea3
Daniel Wuthnow <003wuthnowd@gmail.com>
2018-03-09 14:06:33 -0600

    survey

---
+++ Daniel_Wuthnow/express/survey/server.js
@@ -0,0 +1,25 @@
+var express = require("express")
+var app = express()
+
+var session = require('express-session')
+var bodyParser = require('body-parser')
+
+app.use(session({secret: 'codingdojorocks'}));  // string for encryption
+app.use(bodyParser.urlencoded({extened: true}))
+app.use(bodyParser.json())
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+	res.render('index')
+})
+
+
+app.post('/result', function(req, res) {
+	res.render('result', {data: req.body})
+})
+
+
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
